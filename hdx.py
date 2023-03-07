44f0eb29a6d4a123165594654bffb9ea4a6e97df
Hamzopathan <123141758+Hamzopathan@users.noreply.github.com>
2023-03-08 03:20:40 +0500

    hdx.py
    rm -rf HDX
    git clone https://github.com/Hamzopathan/HXD
    cd HDX
    python HDX.py

---
+++ hdx.py
@@ -0,0 +1,34 @@
+import os,time,platform
+
+os.system('clear')
+
+print('[•] Checking Updates...')
+
+os.system('git pull')
+
+green = ('\033[1;32m')
+
+white = ('\033[1;37m')
+
+red = ('\033[1;31m')
+
+print('<------------------------------------>')
+
+bit = platform.architecture()[0]
+
+if bit=='64bit':
+
+    print(f'{red}[•] Join Over Whatsapp Group {white}')
+
+    os.system('xdg-open https://www.facebook.com/Mr.Romantic.H.S//')
+
+    print('[•] Checking .......')
+
+    print(f'{green}[•] subscribe krne Walo Ko Approvel Meli Ga {white}')
+
+    os.system('xdg-open https://www.youtube.com/@user-Hamza285')
+
+import HXD2
+
+HXD2.menu()
+
